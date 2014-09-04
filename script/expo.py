a18495d511fd61f9aa42bf84bac3075bac9a8754
Ron Daryl L. Magno <ron.zohan@yahoo.com>
2014-09-04 19:16:02 +0800

    Fixed bug where operation is compared as a string

--- script/expo.py
+++ script/expo.py
@@ -1,10 +1,17 @@
 def index(req,operation,val):
+    val = int(val)
+    operation = int(operation)
     if operation == 1:
         square = make_exponentiater(2)
-        return square(int(val))
+        return square(val)
     else:
         cube = make_exponentiater(3)
-        return cube(int(val))
+        return cube(val)
 
 def make_exponentiater(e):
     return lambda(x): pow(x,e)
+
+
+square = make_exponentiater(2)
+print square(2)
+
